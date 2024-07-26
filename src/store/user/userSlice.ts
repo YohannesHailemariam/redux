56a5e56418b960f3c3152ade293557e9b150da0e
iamjohnhm <jhm90822@gmail.com>
2024-07-26 15:27:37 +0300

    Created a reducer named updateUsername in the userSlice file

--- src/store/user/userSlice.ts
+++ src/store/user/userSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface userState {
     id: number;
@@ -16,6 +16,11 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-         
-    }
-}) 
+        updateUsername: (state, action: PayloadAction<string>) => {
+            state.username = action.payload;
+        },
+    },
+});
+
+export const { updateUsername } = userSlice.actions;
+export default userSlice.reducer;
